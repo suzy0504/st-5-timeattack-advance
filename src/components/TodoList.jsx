688ec835fc896662d45bf9d299986ae8ff04b122
suzy <tnwl9540@gmail.com>
2024-06-24 16:41:54 +0900

    11

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
@@ -20,35 +20,49 @@ export default function TodoList() {
 
   // TODO: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
-      queryClient.setQueryData(["todos"], (prev) =>
-        prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
+
+  const handleLike = useMutation({
+    mutationFn: (id, currentLiked) => todoApi.patch(`/todos/${id}`),
+    onMutate: async (id, currentLiked) => {
+      await queryClient.cancelQueries({ queryKey: ["queryKey"] });
+      const previousTodos = [...todos];
+      queryClient.setQueryData(["queryKey"], (prew) => !prev);
+      return { previousTodos };
+    },
+  });
+
+  useMutation({
+    mutationFn: handleLike,
+    onMutate: async (id, currentLiked) => {
+      const previousTodos = [...todos];
+      try {
+        queryClient.setQueryData(["todos"], (prev) =>
+          prev.map((todo) =>
+            todo.id === id ? { ...todo, liked: !todo.liked } : todo
+          )
+        );
+        await todoApi.patch(`/todos/${id}`, {
+          liked: !currentLiked,
+        });
+      } catch (err) {
+        console.error(err);
+        queryClient.setQueryData(["todos"], previousTodos);
+      } finally {
+        refetch();
+      }
+    },
+
+    if(isPending) {
+      return <div style={{ fontSize: 36 }}>로딩중...</div>;
+    },
+
+    if(error) {
+      console.error(error);
+      return (
+        <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
       );
-      await todoApi.patch(`/todos/${id}`, {
-        liked: !currentLiked,
-      });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
-    }
-  };
-
-  if (isPending) {
-    return <div style={{ fontSize: 36 }}>로딩중...</div>;
-  }
-
-  if (error) {
-    console.error(error);
-    return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
-    );
-  }
+    },
+  });
 
   return (
     <ul style={{ listStyle: "none", width: 250 }}>
